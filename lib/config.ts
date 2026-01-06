04de8226b6e5bc81fec406fe496e0de628b3340b
surfjunkieclub <surfjunkieclub@gmail.com>
2026-01-06 18:09:44 -0300

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -21,6 +21,7 @@ export const STARTER_PROMPTS: StartScreenPrompt[] = [
     prompt: "WAVE POOL SESH: BOOK NOW",
     icon: "circle-question",
   },
+  {
 ];
 
 export const PLACEHOLDER_INPUT = "Go ahead...";
